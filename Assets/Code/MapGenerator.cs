55c6b4449dc928f19b28a59176d45544881c418d
TomBarstow <64477329+TomBarstow@users.noreply.github.com>
2022-10-25 16:23:07 -0400

    Expanded procedural generation
    Includes spawn area and boundary wall

--- Assets/Code/MapGenerator.cs
+++ Assets/Code/MapGenerator.cs
@@ -7,30 +7,98 @@ public class MapGenerator : MonoBehaviour
     public GameObject[] tiles;
     public int gridx;
     public int gridz;
-    public float gridSpacing = 1f;
+    public int spawnArea;
+    public int gridSpacing = 10;
     public Vector3 gridOrigin = Vector3.zero;
 
     void Start()
     {
         GenerateMap();
+        GenerateBoundary();
     }
 
     void GenerateMap()
     {
-        for (int x = 0; x < gridx; x++)
+        gridx = gridx / 2;
+        gridz = gridz / 2;
+        int startx = -(gridx);
+        int startz = -(gridz);
+
+        for (int x = startx; x <= gridx; x++)
         {
-            for (int z = 0; z < gridz; z++)
+            for (int z = startz; z <= gridz; z++)
             {
                 Vector3 spawnPosition = new Vector3(x * gridSpacing, 0, z * gridSpacing) + gridOrigin;
-                PickAndSpawn(spawnPosition, Quaternion.identity);
+
+                if (-spawnArea <= spawnPosition.x && spawnPosition.x <= spawnArea)
+                {
+                    if (-spawnArea <= spawnPosition.z && spawnPosition.z <= spawnArea)
+                    {
+                        SpawnAreaSpawn(spawnPosition, Quaternion.identity);
+                    }
+                    else
+                    {
+                        PickAndSpawn(spawnPosition, Quaternion.identity);
+                    }
+                }
+                else
+                {
+                    PickAndSpawn(spawnPosition, Quaternion.identity);
+                }
             }
         }
     }
+
+    void GenerateBoundary()
+    {
+        int boundaryx = gridx + 2;
+        int boundaryz = gridz + 2;
+
+        int mapAreax = gridx * gridSpacing;
+        int mapAreaz = gridz * gridSpacing;
+
+        int startx = -(boundaryx - 1);
+        int startz = -(boundaryz - 1);
+
+        for (int x = startx; x < boundaryx; x++)
+        {
+            for (int z = startz; z < boundaryz; z++)
+            {
+                Vector3 spawnPosition = new Vector3(x * gridSpacing, 0, z * gridSpacing) + gridOrigin;
+                if (-mapAreaz <= spawnPosition.z && spawnPosition.z <= mapAreaz)
+                {
+                    if (-mapAreax <= spawnPosition.x && spawnPosition.x <= mapAreax)
+                    {
+                        
+                    }
+                    else
+                    {
+                        BoundarySpawn(spawnPosition, Quaternion.identity);
+                    }
+                }
+                else
+                {
+                    BoundarySpawn(spawnPosition, Quaternion.identity);
+                }
+            }
+        }
+    }
+
 
     void PickAndSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
     {
         int randomIndex = Random.Range(0, tiles.Length);
         GameObject clone = Instantiate(tiles[randomIndex], positionToSpawn, rotationToSpawn);
+    }
+
+    void SpawnAreaSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
+    {
+        GameObject clone = Instantiate(tiles[0], positionToSpawn, rotationToSpawn);
+    }
+
+    void BoundarySpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
+    {
+        GameObject clone = Instantiate(tiles[1], positionToSpawn, rotationToSpawn);
     }
 
 
