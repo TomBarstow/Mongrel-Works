2327a1195d47c16a88714da82d0e039d31a33453
TomBarstow <64477329+TomBarstow@users.noreply.github.com>
2022-11-08 13:36:10 -0500

    Broke everything

--- Assets/Code/MapGenerator.cs
+++ Assets/Code/MapGenerator.cs
@@ -5,100 +5,95 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     public GameObject[] tiles;
-    public int mapWidth;
-    public int mapHeight;
-    //public int mapFloors;
-    public int spawnSquareArea;
+    public int gridx;
+    public int gridz;
+    public int spawnArea;
     public int gridSpacing = 10;
-    public Vector3 gridOrigin = Vector3.zero;
+    //private Vector3 gridOrigin = Vector3.zero;
+
+    public int[,] gridArray;
 
     //Offset for perlin noise map
-    Vector3 shift = new Vector3(237, 0, 92);
+    public Vector3 shift = new Vector3(237, 0, 92);
     //Zoom for perlin noise map
-    float zoom = 0.05f;
+    public float zoom = 0.1f;
 
     void Start()
     {
+        GenerateArray();
         GenerateMap();
     }
 
+    void GenerateArray()
+    {
+        gridArray = new int[gridx, gridz];
+
+        //TODO add code to offset spawn area to center or array and create references for logic
+
+        for (int x = 0; x <= gridx - 1; x++)
+        {
+            for (int z = 0; z <= gridz - 1; z++)
+            {
+
+                Vector3 pos = zoom * (new Vector3(x, 0, z)) + shift;
+                float noise = Mathf.PerlinNoise(pos.x, pos.z);
+
+                if (-spawnArea <= x & x <= spawnArea & -spawnArea <= z & z <= spawnArea)
+                {
+                    gridArray[x,z] = 2;
+                }
+                else if (noise < 0.4 || noise > 0.6)
+                {
+                    gridArray[x,z] = 0;
+                }
+                else
+                {
+                    gridArray[x,z] = 1;
+                }
+                Debug.Log(gridArray[x,z]);
+            }
+        }
+    }
 
     void GenerateMap()
     {
-        int gridx = mapWidth / 2;
-        //int gridy = mapFloors / 2;
-        int gridz = mapHeight / 2;
-        int startx = -(gridx);
-        int startz = -(gridz);
-        int mapAreax = gridx * gridSpacing;
-        int mapAreaz = gridz * gridSpacing;
-        int spawnArea = (spawnSquareArea * gridSpacing)/2;
+        Vector3 gridOrigin = new Vector3(-((gridx * gridSpacing) / 2), 0, -((gridz * gridSpacing) / 2));
+        //Vector3 gridOrigin = new Vector3(0,0,0);
 
-        int shiftNoise = Random.Range((mapWidth + mapHeight), 100000);
-        shift = new Vector3(shiftNoise, 0, shiftNoise);
-
-        for (int x = startx; x <= gridx; x++)
+        for (int x = 0; x <= gridx - 1; x++)
+        {
+            for (int z = 0; z <= gridz - 1; z++)
             {
-                for (int z = startz; z <= gridz; z++)
-                {
-                    //Spawn position
-                    Vector3 spawnPosition = new Vector3(x * gridSpacing, 0, z * gridSpacing) + gridOrigin;
-                    //Perlin noise value
-                    Vector3 pos = zoom * (new Vector3(x, 0, z)) + shift;
-                    float noise = Mathf.PerlinNoise(pos.x, pos.z);
-                    //Debug.Log(noise);
+                //Spawn position
+                Vector3 spawnPosition = new Vector3(x * gridSpacing, 0, z * gridSpacing) + gridOrigin;
 
-                    //Condition for spawn area fill
-                    if (-spawnArea <= spawnPosition.x && spawnPosition.x <= spawnArea && -spawnArea <= spawnPosition.z && spawnPosition.z <= spawnArea)
-                    {
-                        SpawnAreaSpawn(spawnPosition, Quaternion.identity);
-                    }
-                    //Condition for boundary walls
-                    else if (spawnPosition.x == -mapAreax || spawnPosition.x == mapAreax || spawnPosition.z == -mapAreaz || spawnPosition.z == mapAreaz)
-                    {
-                        BoundarySpawn(spawnPosition, Quaternion.identity);
-                    }
-                    //Default condition for random area fill
-                    else
-                    {
-                        if (noise < 0.2f)
-                        {
-                            WallSpawn(spawnPosition, Quaternion.identity);
-                        }
-                        else if (noise > 0.2f && noise < 0.35f)
-                        {
-                            RandomAreaSpawn(spawnPosition, Quaternion.identity);
-                        }
-                        else if (noise > 0.35f && noise < 0.5f)
-                        {
-                            GrassAreaSpawn(spawnPosition, Quaternion.identity);
-                        }
-                        else if (noise > 0.5f && noise < 0.62f)
-                        {
-                            RandomAreaSpawn(spawnPosition, Quaternion.identity);
-                        }
-                        else if (noise > 0.62f && noise < 0.65f)
-                        {
-                            PillarSpawn(spawnPosition, Quaternion.identity);
-                        }
-                        else if (noise > 0.65f)
+                switch (gridArray[x, z])
+                {
+                    case 0:
+                        /*
+                        //TODO make looping function to check for neighbors in array
+                        //This is incrementing the variables... pretty dumb ngl
+                        if (x + 1 == 2 || x + 1 == 1 || x - 1 == 2 || x - 1 == 1 || z + 1 == 2 || z + 1 == 1 || z - 1 == 2 || z - 1 == 1)
                         {
                             BoundarySpawn(spawnPosition, Quaternion.identity);
                         }
-                    }
+                        */
+                        BoundarySpawn(spawnPosition, Quaternion.identity);
+                        break;
 
+                    case 1:
+                        GrassAreaSpawn(spawnPosition, Quaternion.identity);
+                        break;
+
+                    case 2:
+                        SpawnAreaSpawn(spawnPosition, Quaternion.identity);
+                        break;
                 }
+
             }
-
-        
+        }
     }
 
-    void RandomAreaSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
-    {
-        int randomIndex = Random.Range(3, tiles.Length);
-        GameObject clone = Instantiate(tiles[randomIndex], positionToSpawn, rotationToSpawn);
-        clone.transform.localRotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 4) * 90, 0));
-    }
 
     void SpawnAreaSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
     {
@@ -108,23 +103,10 @@ public class MapGenerator : MonoBehaviour
     void BoundarySpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
     {
         GameObject clone = Instantiate(tiles[1], positionToSpawn, rotationToSpawn);
-        //makes more taller-er
-        clone.transform.localScale = new Vector3(1, 3, 1);
-        //clone.transform.position = new Vector3(positionToSpawn.x, positionToSpawn.y + 0.0f, positionToSpawn.z);
-    }
-
-    void WallSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
-    {
-        GameObject clone = Instantiate(tiles[1], positionToSpawn, rotationToSpawn);
-    }
-
-    void PillarSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
-    {
-        GameObject clone = Instantiate(tiles[2], positionToSpawn, rotationToSpawn);
     }
 
     void GrassAreaSpawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
     {
-        GameObject clone = Instantiate(tiles[3], positionToSpawn, rotationToSpawn);
+        GameObject clone = Instantiate(tiles[2], positionToSpawn, rotationToSpawn);
     }
 }
